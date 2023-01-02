9c569ec11ead276ece1558531c8bdfc39611af6e
Yatin Nagpal <nnega4428@gmail.com>
2023-01-03 09:21:25 +1100

    set up routes and view page for movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  get 'movies/index'
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :movies, only: :index
 end
